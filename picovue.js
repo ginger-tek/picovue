52af2b9c3df16d59f53d247f3a0dde638a1d5462
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2024-07-18 17:38:47 -0400

    reuse install export on modules

--- picovue.js
+++ picovue.js
@@ -1,18 +1,17 @@
-import { PvTable } from './picovue.table.js'
-import { PvToaster } from './picovue.toaster.js'
-import { PvModal } from './picovue.modal.js'
-import { PvTab, PvTabs } from './picovue.tabs.js'
-import { PvDarkMode } from './picovue.dark-mode.js'
-import { PvDropdown } from './picovue.dropdown.js'
+import PvTable from './pico-vue.table.js'
+import PvToaster from './pico-vue.toaster.js'
+import PvModal from './pico-vue.modal.js'
+import PvTabs from './pico-vue.tabs.js'
+import PvDarkMode from './pico-vue.dark-mode.js'
+import PvDropdown from './pico-vue.dropdown.js'
 
 export default {
   install(app) {
-    app.component('pv-table', PvTable)
-    app.component('pv-toaster', PvToaster)
-    app.component('pv-modal', PvModal)
-    app.component('pv-tab', PvTab)
-    app.component('pv-tabs', PvTabs)
-    app.component('pv-dark-mode', PvDarkMode)
-    app.component('pv-dropdown', PvDropdown)
+    PvTable.install(app)
+    PvToaster.install(app)
+    PvModal.install(app)
+    PvTabs.install(app)
+    PvDarkMode.install(app)
+    PvDropdown.install(app)
   }
 }
