a9b72660f7a8560971164501996c0d62a8377e0a
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2024-07-18 22:40:17 -0400

    fix file names

--- picovue.js
+++ picovue.js
@@ -1,9 +1,9 @@
-import PvTable from './pico-vue.table.js'
-import PvToaster from './pico-vue.toaster.js'
-import PvModal from './pico-vue.modal.js'
-import PvTabs from './pico-vue.tabs.js'
-import PvDarkMode from './pico-vue.dark-mode.js'
-import PvDropdown from './pico-vue.dropdown.js'
+import PvTable from './picovue.table.js'
+import PvToaster from './picovue.toaster.js'
+import PvModal from './picovue.modal.js'
+import PvTabs from './picovue.tabs.js'
+import PvDarkMode from './picovue.dark-mode.js'
+import PvDropdown from './picovue.dropdown.js'
 
 export default {
   install(app) {
