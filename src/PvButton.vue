a7f5363dcd11e0971a13a7ce473a47e5c95b90a0
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-26 03:50:16 +0000

    new confirm and button components

---
+++ src/PvButton.vue
@@ -0,0 +1,89 @@
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: null,
+    validator(value) {
+      return [
+        null,
+        'success',
+        'info',
+        'warning',
+        'danger'
+      ].includes(value)
+    }
+  },
+  busy: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <button :class="['pv-btn', type]" :aria-busy="busy" :disabled="busy">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button.pv-btn.success {
+  --pico-background-color: var(--pv-green);
+  --pico-border-color: var(--pv-green);
+  --pico-color: white;
+
+  &:is(:hover, :focus, :active) {
+    --pico-border-color: var(--pv-green-hover);
+    --pico-background-color: var(--pv-green-hover);
+
+    &:focus {
+      --pico-box-shadow: var(--pico-button-hover-box-shadow, 0 0 0 rgba(0, 0, 0, 0)), 0 0 0 var(--pico-outline-width) var(--pv-green-box-shadow);
+    }
+  }
+}
+
+button.pv-btn.danger {
+  --pico-background-color: var(--pv-red);
+  --pico-border-color: var(--pv-red);
+  --pico-color: white;
+
+  &:is(:hover, :focus, :active) {
+    --pico-border-color: var(--pv-red-hover);
+    --pico-background-color: var(--pv-red-hover);
+
+    &:focus {
+      --pico-box-shadow: var(--pico-button-hover-box-shadow, 0 0 0 rgba(0, 0, 0, 0)), 0 0 0 var(--pico-outline-width) var(--pv-red-box-shadow);
+    }
+  }
+}
+
+button.pv-btn.warning {
+  --pico-background-color: var(--pv-amber);
+  --pico-border-color: var(--pv-amber);
+  --pico-color: black;
+
+  &:is(:hover, :focus, :active) {
+    --pico-border-color: var(--pv-amber-hover);
+    --pico-background-color: var(--pv-amber-hover);
+
+    &:focus {
+      --pico-box-shadow: var(--pico-button-hover-box-shadow, 0 0 0 rgba(0, 0, 0, 0)), 0 0 0 var(--pico-outline-width) var(--pv-amber-box-shadow);
+    }
+  }
+}
+
+button.pv-btn.info {
+  --pico-background-color: var(--pv-cyan);
+  --pico-border-color: var(--pv-cyan);
+  --pico-color: white;
+
+  &:is(:hover, :focus, :active) {
+    --pico-border-color: var(--pv-cyan-hover);
+    --pico-background-color: var(--pv-cyan-hover);
+
+    &:focus {
+      --pico-box-shadow: var(--pico-button-hover-box-shadow, 0 0 0 rgba(0, 0, 0, 0)), 0 0 0 var(--pico-outline-width) var(--pv-cyan-box-shadow);
+    }
+  }
+}
+</style>
---
+++ src/PvConfirm.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+
+const confirmEl = ref(null)
+const resolution = ref(null)
+const message = ref(null)
+
+async function confirmAsync(msg = '') {
+  resolution.value = null
+  message.value = msg
+  confirmEl.value.showModal()
+  return new Promise(res => {
+    watch(() => resolution.value, (n) => {
+      if (n !== null) {
+        res(n)
+        confirmEl.value.close()
+      }
+    })
+  })
+}
+
+onMounted(() => {
+  confirmEl.value.addEventListener('close', () => resolution.value = false)
+  confirmEl.value.addEventListener('cancel', () => resolution.value = false)
+})
+
+defineExpose({ confirmAsync })
+</script>
+
+<template>
+  <dialog ref="confirmEl">
+    <article>
+      <header>
+        <div><b>Confirm</b></div>
+      </header>
+      {{ message }}
+      <footer>
+        <button @click="resolution = false" class="secondary">No</button>
+        <button @click="resolution = true">Yes</button>
+      </footer>
+    </article>
+  </dialog>
+</template>
